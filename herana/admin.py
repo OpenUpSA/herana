cd1cce7ca07e5a3968f5eaea808db757dc60d533
xybrnet <damian@code4sa.org>
2015-06-23 09:36:47 +0200

    Register models in admin

---
+++ herana/admin.py
@@ -0,0 +1,13 @@
+from django.utils.translation import ugettext_lazy as _
+from django.contrib import admin
+
+import models
+
+admin.site.register(models.Institute)
+admin.site.register(models.Faculty)
+admin.site.register(models.StrategicObjective)
+admin.site.register(models.InstituteAdmin)
+admin.site.register(models.ProjectLeader)
+admin.site.register(models.ReportingPeriod)
+admin.site.register(models.ProjectHeader)
+admin.site.register(models.ProjectDetail)
