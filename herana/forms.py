81bd740e60ce850d1617d2323b6e65960129ef0f
xybrnet <damian@code4sa.org>
2015-07-01 12:24:39 +0200

    Fix check for _draft key in request object

--- herana/forms.py
+++ herana/forms.py
@@ -10,7 +10,7 @@ class ProjectDetailForm(forms.ModelForm):
 
     def _clean_fields(self):
         # If we are saving a draft, only the header field is required.
-        if self.data['_draft']:
+        if '_draft' in self.data:
             for name, field in self.fields.items():
                 if not name == 'header':
                     field.required = False
