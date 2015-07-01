36e46dce884b1bbcb1a11cbdff9cca7810787d7b
Greg Kempe <greg@kempe.net>
2015-07-01 14:30:23 +0200

    Permanent redirect for /

--- herana/urls.py
+++ herana/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
+
 
 urlpatterns = patterns('',
-    url(r'^$', 'herana.views.home', name='home'),
+    url(r'^$', RedirectView.as_view(url='/admin/', permanent=True), name='home'),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^accounts/', include('registration.backends.default.urls')),
