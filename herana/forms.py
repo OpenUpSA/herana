7bc85f079219eab63108791c33983c66d4bf4372
xybrnet <damian@code4sa.org>
2015-07-02 17:35:55 +0200

    Fix custom form validation

--- herana/forms.py
+++ herana/forms.py
@@ -1,7 +1,7 @@
 from django.contrib.auth.models import User
 from django import forms
 
-from models import ProjectDetail
+from models import ProjectDetail, ProjectFunding, PHDStudent
 
 class ProjectDetailForm(forms.ModelForm):
     class Meta:
@@ -16,59 +16,59 @@ class ProjectDetailForm(forms.ModelForm):
                     field.required = False
         super(ProjectDetailForm, self)._clean_fields()
 
-    def clean_end_date(self):
-        if self.cleaned_data['project_status'] == 1 and self.cleaned_data['end_date'] == None:
+    def clean(self):
+        cleaned_data = super(ProjectDetailForm, self).clean()
+
+        if cleaned_data.get('project_status') == 1 and cleaned_data.get('end_date') == None:
             msg = "A complete project must have an end date."
             self.add_error('end_date', msg)
 
-    def clean_focus_area_text(self):
-        for choice in self.cleaned_data['focus_area']:
-            if choice.code == 4 and self.cleaned_data['focus_area_text'] == '':
-                msg = "If other was chosen above, please describe."
-                self.add_error('focus_area_text', msg)
+        if cleaned_data.get('end_date') and cleaned_data.get('start_date'):
+            if cleaned_data.get('end_date') > cleaned_data.get('start_date'):
+                msg = "The project end date cannot be before the project start date."
+                self.add_error('end_date', msg)
 
-    def clean_strategic_objectives(self):
-        if self.cleaned_data['strategic_objectives'].exists():
-            if len(self.cleaned_data['strategic_objectives']) != 4:
+        if cleaned_data.get('focus_area'):
+            for choice in cleaned_data.get('focus_area'):
+                if choice.code == 4 and self.cleaned_data.get('focus_area_text') == '':
+                    msg = "If other was chosen above, please describe."
+                    self.add_error('focus_area_text', msg)
+
+        if cleaned_data.get('strategic_objectives'):
+            if len(self.cleaned_data['strategic_objectives']) > 4:
                 msg = "Please select only 4 options."
                 self.add_error('strategic_objectives', msg)
 
-    def clean_public_domain_url(self):
-        if self.cleaned_data['public_domain'] == 'Y' and self.cleaned_data['public_domain_url'] == '':
+        if cleaned_data.get('public_domain') == 'Y' and cleaned_data.get('public_domain_url') == '':
             msg = "If yes was selected above, please provide the URL."
             self.add_error('public_domain_url', msg)
 
-    def clean_new_initiative_text(self):
-        if self.cleaned_data['new_initiative'] == 'Y' and self.cleaned_data['new_initiative_text'] == '':
+        if cleaned_data.get('new_initiative') == 'Y' and cleaned_data.get('new_initiative_text') == '':
             msg = "If yes was selected above, please describe."
             self.add_error('new_initiative_text', msg)
 
-    def clean_new_initiative_party_text(self):
-        if self.cleaned_data['new_initiative_party'] == 1 and self.cleaned_data['new_initiative_party_text'] == '':
+        if cleaned_data.get('new_initiative_party') == 1 and cleaned_data('new_initiative_party_text') == '':
             msg = "Please provide the name of the third party."
             self.add_error('new_initiative_party_text', msg)
-        if self.cleaned_data['new_initiative_party'] == 3 and self.cleaned_data['new_initiative_party_text'] == '':
+
+        if cleaned_data.get('new_initiative_party') == 3 and cleaned_data.get('new_initiative_party_text') == '':
             msg = "Please describe how the product / service / intervention / policy will be developed and/or implemented."
             self.add_error('new_initiative_party_text', msg)
 
-    def clean_research_text(self):
-        if not self.cleaned_data['research'] is None and self.cleaned_data['research_text'] == '':
+        if not cleaned_data.get('research') is None and cleaned_data.get('research_text') == '':
             msg = "Please provide a description for your choice in the field above."
             self.add_error('research_text', msg)
 
-    def clean_curriculum_changes_text(self):
-        if self.cleaned_data['curriculum_changes'] == 'Y' and self.cleaned_data['curriculum_changes_text'] == '':
+        if cleaned_data.get('curriculum_changes') == 'Y' and cleaned_data.get('curriculum_changes_text') == '':
             msg = "Please describe the types of changes made to the curriculum as a result of the project."
             self.add_error('curriculum_changes_text', msg)
 
-    def clean_student_types(self):
-        if self.cleaned_data['students_involved'] == 'Y' and not self.cleaned_data['student_types'].exists():
+        if cleaned_data.get('students_involved') == 'Y' and not cleaned_data.get('student_types'):
             msg = "Please indicate the types of students involved."
             self.add_error('student_types', msg)
 
-    def clean_student_nature_text(self):
-        if self.cleaned_data['student_nature'] == 6 and self.cleaned_data['student_nature_text'] == 'Y':
-            msg = "Please describe the nature of student participation."
-            self.add_error('student_types', msg)
-
-
+        if cleaned_data.get('student_nature'):
+            for item in cleaned_data.get('student_nature'):
+                if item.code == 6 and cleaned_data.get('student_nature_text') == '':
+                    msg = "Please describe the nature of student participation."
+                    self.add_error('student_nature_text', msg)
