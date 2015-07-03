937a6c10746a0603fbe59e024d465d58395d781a
xybrnet <damian@code4sa.org>
2015-07-03 17:03:46 +0200

    Update migrations

--- herana/migrations/0001_initial.py
+++ herana/migrations/0001_initial.py
@@ -2,16 +2,37 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import django.utils.timezone
 from django.conf import settings
 
 
 class Migration(migrations.Migration):
 
     dependencies = [
-        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
+        ('auth', '0006_require_contenttypes_0002'),
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='CustomUser',
+            fields=[
+                ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                ('password', models.CharField(max_length=128, verbose_name='password')),
+                ('last_login', models.DateTimeField(null=True, verbose_name='last login', blank=True)),
+                ('is_superuser', models.BooleanField(default=False, help_text='Designates that this user has all permissions without explicitly assigning them.', verbose_name='superuser status')),
+                ('email', models.EmailField(unique=True, max_length=254)),
+                ('first_name', models.CharField(max_length=30, verbose_name='first name', blank=True)),
+                ('last_name', models.CharField(max_length=30, verbose_name='last name', blank=True)),
+                ('is_staff', models.BooleanField(default=False, help_text='Designates whether the user can log into this admin site.', verbose_name='staff status')),
+                ('is_active', models.BooleanField(default=True, help_text='Designates whether this user should be treated as active. Unselect this instead of deleting accounts.', verbose_name='active')),
+                ('date_joined', models.DateTimeField(default=django.utils.timezone.now, verbose_name='date joined')),
+                ('groups', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Group', blank=True, help_text='The groups this user belongs to. A user will get all permissions granted to each of their groups.', verbose_name='groups')),
+                ('user_permissions', models.ManyToManyField(related_query_name='user', related_name='user_set', to='auth.Permission', blank=True, help_text='Specific permissions for this user.', verbose_name='user permissions')),
+            ],
+            options={
+                'abstract': False,
+            },
+        ),
         migrations.CreateModel(
             name='AdvisoryGroupRep',
             fields=[
@@ -89,16 +110,16 @@ class Migration(migrations.Migration):
             name='ProjectDetail',
             fields=[
                 ('id', models.AutoField(verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
-                ('name', models.CharField(max_length=512, verbose_name='Please select the engagement project for which you wish to capture the details.')),
+                ('name', models.CharField(max_length=512, verbose_name='Please enter the name of the engagement project for which you wish to capture the details.')),
                 ('date_created', models.DateField(auto_now_add=True)),
                 ('is_leader', models.CharField(max_length=1, null=True, verbose_name='Are you the designated leader of this engagement project?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('is_flagship', models.CharField(max_length=1, null=True, verbose_name='Do you consider this to be your flagship engagement project?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('project_status', models.PositiveIntegerField(null=True, verbose_name='Project status', choices=[(1, 'Complete'), (2, 'Ongoing')])),
                 ('start_date', models.DateField(null=True, verbose_name='Start date')),
-                ('end_date', models.DateField(null=True, verbose_name='End date')),
+                ('end_date', models.DateField(null=True, verbose_name='End date', blank=True)),
                 ('multi_faculty', models.CharField(max_length=1, null=True, verbose_name='Does the project span multiple faculties/schools?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('description', models.TextField(null=True, verbose_name='Please provide a short description of the project.')),
-                ('focus_area_text', models.CharField(max_length=256, null=True, verbose_name='If other was chosen above, please describe.')),
+                ('focus_area_text', models.CharField(max_length=256, null=True, verbose_name='If other was chosen above, please describe.', blank=True)),
                 ('classification', models.PositiveIntegerField(null=True, verbose_name='How would you classify this flagship engagement activity?', choices=[(1, 'Project'), (2, 'Programme'), (3, 'Service'), (4, 'Other')])),
                 ('outcomes', models.TextField(null=True, verbose_name='What are the planned outcomes of the project?')),
                 ('beneficiaries', models.TextField(null=True, verbose_name='Who are the intended beneficiaries of the project?')),
@@ -106,27 +127,27 @@ class Migration(migrations.Migration):
                 ('authors', models.PositiveIntegerField(null=True, verbose_name='How many authors were involved in the writing of the project proposal/plan?', choices=[(1, 'One author'), (2, 'More than one author')])),
                 ('amendments_permitted', models.CharField(max_length=1, null=True, verbose_name='To the best of your knowledge, are deviations or amendments to the request for proposal permitted by the funder?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('public_domain', models.CharField(help_text=b'If yes, please provide the URL', max_length=1, null=True, verbose_name='Will the written up findings or data or other outputs of the project be available electronically in the public domain?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
-                ('public_domain_url', models.URLField(null=True, verbose_name='URL')),
+                ('public_domain_url', models.URLField(null=True, verbose_name='URL', blank=True)),
                 ('adv_group', models.CharField(max_length=1, null=True, verbose_name='Does the project have an advisory group?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('adv_group_freq', models.PositiveIntegerField(null=True, verbose_name='How often does the advisory group meet?', choices=[(1, 'Monthly'), (2, 'Quarterly'), (3, 'Annually'), (4, 'Ad hoc')])),
                 ('new_initiative', models.CharField(max_length=1, null=True, verbose_name='Will any of the findings/results/outcomes of the project be used to implement or develop new products/services/interventions/policies?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
-                ('new_initiative_text', models.TextField(null=True, verbose_name='If yes, please describe')),
+                ('new_initiative_text', models.TextField(null=True, verbose_name='If yes, please describe', blank=True)),
                 ('new_initiative_party', models.PositiveIntegerField(null=True, verbose_name='Who will implement or develop these new products/services/interventions/policies?', choices=[(1, 'A third party has been contracted to develop the product / service / intervention / policy'), (2, 'The project team will develop the product / service / intervention / policy'), (3, 'Other')])),
                 ('new_initiative_party_text', models.TextField(null=True, verbose_name='Please provide the name of the third party or describe how the product / service / intervention / policy will be developed and/or implemented')),
                 ('research', models.PositiveIntegerField(null=True, verbose_name='In terms of the research done for the project (if applicable), which statement best describes the project', choices=[(1, 'The project conducted original research'), (2, 'The project collected new data and conducted original research to complete the project'), (3, 'The project collected new data and applied existing knowledge to the data collected to complete the project'), (4, 'No new data was required and existing knowledge was applied to complete the project'), (5, 'Not applicable')])),
-                ('research_text', models.TextField(help_text='If <b>1, 2 or 3</b> selected, please provide a short description of the data collected or the original research conducted <br> If <b>4 or 5</b> selected, please explain why it was not necessary to collect new data or conduct original research', null=True, verbose_name='Please describe')),
+                ('research_text', models.TextField(help_text='If <b>1, 2 or 3</b> selected, please provide a short description of the data collected or the original research conducted <br> If <b>4 or 5</b> selected, please explain why it was not necessary to collect new data or conduct original research', null=True, verbose_name='Please describe', blank=True)),
                 ('phd_research', models.CharField(max_length=1, null=True, verbose_name="Is a student's PhD research linked to this project?", choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('curriculum_changes', models.CharField(max_length=1, null=True, verbose_name='Has the project lead to any contributions or changes to the curriculum?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
-                ('curriculum_changes_text', models.TextField(null=True, verbose_name='If yes, please describe the types of changes made to the curriculum as a result of the project')),
+                ('curriculum_changes_text', models.TextField(null=True, verbose_name='If yes, please describe the types of changes made to the curriculum as a result of the project', blank=True)),
                 ('new_courses', models.CharField(max_length=1, null=True, verbose_name='Have any new courses/modules/programmes been introduced as a result of this project?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('students_involved', models.CharField(max_length=1, null=True, verbose_name='Are students involved in the project?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
-                ('student_nature_text', models.CharField(max_length=128, null=True, verbose_name='If other was selected, please describe.')),
+                ('student_nature_text', models.CharField(max_length=128, null=True, verbose_name='If other was selected, please describe.', blank=True)),
                 ('course_requirement', models.CharField(max_length=1, null=True, verbose_name='Is participation in the project a requirement for the completion of an existing course/degree offered by the university?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('external_collaboration', models.CharField(max_length=1, null=True, verbose_name='Did you collaborate with academics from other universities on the project?', choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('record_status', models.PositiveIntegerField(choices=[(1, b'Draft'), (2, b'Final')])),
                 ('rejected', models.BooleanField(default=False)),
                 ('rejected_detail', models.TextField(null=True)),
-                ('adv_group_rep', models.ManyToManyField(to='herana.AdvisoryGroupRep', verbose_name='If yes, are any of the following members of the advisory group?')),
+                ('adv_group_rep', models.ManyToManyField(to='herana.AdvisoryGroupRep', verbose_name='If yes, are any of the following members of the advisory group?', blank=True)),
                 ('faculty', models.ForeignKey(verbose_name=b'In which faculty/school is the project located?', to='herana.Faculty', null=True)),
                 ('focus_area', models.ManyToManyField(help_text='Select ALL applicable<br>', to='herana.FocusArea', verbose_name='In which of the following areas would you place your project?')),
             ],
@@ -143,7 +164,7 @@ class Migration(migrations.Migration):
                 ('funder', models.CharField(max_length=256)),
                 ('amount', models.DecimalField(max_digits=10, decimal_places=2)),
                 ('years', models.DecimalField(max_digits=5, decimal_places=2)),
-                ('renewable', models.BooleanField(choices=[(b'Y', 'Yes'), (b'N', 'No')])),
+                ('renewable', models.CharField(max_length=1, null=True, choices=[(b'Y', 'Yes'), (b'N', 'No')])),
                 ('project', models.ForeignKey(to='herana.ProjectDetail')),
             ],
         ),
@@ -238,7 +259,7 @@ class Migration(migrations.Migration):
         migrations.AddField(
             model_name='projectdetail',
             name='student_types',
-            field=models.ManyToManyField(to='herana.StudentType', verbose_name='If yes, please indicate types of students participating'),
+            field=models.ManyToManyField(to='herana.StudentType', verbose_name='If yes, please indicate types of students participating', blank=True),
         ),
         migrations.AddField(
             model_name='projectdetail',
