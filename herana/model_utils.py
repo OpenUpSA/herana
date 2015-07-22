5db17782b3e0c0d5e65300e8953e0931367f7d5b
Damian <damian@code4sa.org>
2015-07-22 13:21:39 +0200

    Fix Chrome text overflow issue

--- herana/model_utils.py
+++ herana/model_utils.py
@@ -70,9 +70,9 @@ CAPTURE_LABELS = {
     'end_date': _(
         'End date'),
     'faculty': (
-        'P3.0: In which faculty/school is the project located?'),
+        'P3.0: In which faculty / school is the project located?'),
     'multi_faculty': _(
-        'P3.1: Does the project span multiple faculties/schools?'),
+        'P3.1: Does the project span multiple faculties / schools?'),
     'description': _(
         'P4.0: Please provide a short description of the project.'),
     'focus_area': _(
@@ -90,7 +90,7 @@ CAPTURE_LABELS = {
     'initiation': _(
         'A2.1: Which one of the following statements best describes how the project was initiated?'),
     'authors': _(
-        'A2.2: How many authors were involved in the writing of the project proposal/plan?'),
+        'A2.2: How many authors were involved in the writing of the project proposal / plan?'),
     'amendments_permitted': _(
         'A2.3: To the best of your knowledge, are deviations or amendments to the request for proposal permitted by the funder?'),
     'public_domain': _(
@@ -106,13 +106,13 @@ CAPTURE_LABELS = {
     'team_members': _(
         'A3.1: Are any of the following members of the research team?'),
     'new_initiative': _(
-        'A3.2: Will any of the findings/results/outcomes of the project be used to implement or develop new products/services/interventions/policies?'),
+        'A3.2: Will any of the findings / results / outcomes of the project be used to implement or develop new products / services / interventions / policies?'),
     'new_initiative_text': _(
         'If yes, please describe'),
     'new_initiative_party': _(
-        'Who will implement or develop these new products/services/interventions/policies?'),
+        'Who will implement or develop these new products / services / interventions / policies?'),
     'new_initiative_party_text': _(
-        'Please provide the name of the third party or describe how the product / service / intervention / policy will be developed and/or implemented'),
+        'Please provide the name of the third party or describe how the product / service / intervention / policy will be developed and / or implemented'),
     'funding': _(
         'A4.1: Please list sources of project funding, the number of years for which funding has been secured, and the amount of funding (in US$)'),
     'research': _(
@@ -130,7 +130,7 @@ CAPTURE_LABELS = {
     'curriculum_changes_text': _(
         'If yes, please describe the types of changes made to the curriculum as a result of the project'),
     'new_courses': _(
-        'C3.2: Have any new courses/modules/programmes been introduced as a result of this project?'),
+        'C3.2: Have any new courses / modules / programmes been introduced as a result of this project?'),
     'new_course_detail': _(
         'If yes, please describe'),
     'students_involved': _(
@@ -142,7 +142,7 @@ CAPTURE_LABELS = {
     'student_nature_text': _(
         'If other was selected, please describe.'),
     'course_requirement': _(
-        'C3.4: Is participation in the project a requirement for the completion of an existing course/degree offered by the university?'),
+        'C3.4: Is participation in the project a requirement for the completion of an existing course / degree offered by the university?'),
     'course_req_detail': _(
         'If yes, please describe'),
     'external_collaboration': _(
