300ca5de558371f3cd34aa2501b5b307b100fd5d
Damian <damian@code4sa.org>
2015-08-26 20:51:39 +0200

    Fix admin readonly fields

--- herana/admin.py
+++ herana/admin.py
@@ -37,8 +37,27 @@ from forms import ProjectDetailForm, ProjectDetailAdminForm
 ORG_LEVEL_FIELDS = ["org_level_1", "org_level_2", "org_level_3"]
 
 # ------------------------------------------------------------------------------
-# General utility functions
+# General utility classes and functions
 # ------------------------------------------------------------------------------
+
+class ReadOnlyMixin(object):
+    def get_readonly_fields(self, request, obj=None):
+        if user_has_perm(request, self.opts, 'view'):
+            result = list(set(
+                [field.name for field in self.opts.local_fields] +
+                [field.name for field in self.opts.local_many_to_many]
+            ))
+            result.remove('id')
+            return result
+
+    def has_add_permission(self, request):
+        if user_has_perm(request, self.opts, 'view'):
+            return False
+
+    def has_delete_permission(self, request, obj=None):
+        if user_has_perm(request, self.opts, 'view'):
+            return False
+
 
 def user_has_perm(request, opts, perm_type):
     """
@@ -115,7 +134,7 @@ class CollaboratorsFormSet(ProjectDetailFormSet):
 # Inlines
 # ------------------------------------------------------------------------------
 
-class ProjectFundingInline(admin.TabularInline):
+class ProjectFundingInline(ReadOnlyMixin, admin.TabularInline):
     model = ProjectFunding
     extra = 1
     inline_classes = ('grp-collapse grp-open',)
@@ -123,7 +142,7 @@ class ProjectFundingInline(admin.TabularInline):
     verbose_name_plural = _('6.1: Please list sources of project funding, the number of years for which funding has been secured, and the amount of funding (in US$).')
 
 
-class PHDStudentInline(admin.TabularInline):
+class PHDStudentInline(ReadOnlyMixin, admin.TabularInline):
     model = PHDStudent
     formset = PHDStudentFormSet
     extra = 1
@@ -132,7 +151,7 @@ class PHDStudentInline(admin.TabularInline):
     verbose_name_plural = _('7.2.1: If yes, please provide their names.')
 
 
-class ProjectOutputInline(admin.StackedInline):
+class ProjectOutputInline(ReadOnlyMixin, admin.StackedInline):
     model = ProjectOutput
     extra = 1
     inline_classes = ('grp-collapse grp-open',)
@@ -140,7 +159,7 @@ class ProjectOutputInline(admin.StackedInline):
     verbose_name_plural = _('8.1: Please add the completed publications and other outputs for this project.')
 
 
-class NewCourseDetailInline(admin.TabularInline):
+class NewCourseDetailInline(ReadOnlyMixin, admin.TabularInline):
     model = NewCourseDetail
     formset = NewCourseDetailFormSet
     extra = 1
@@ -149,7 +168,7 @@ class NewCourseDetailInline(admin.TabularInline):
     verbose_name_plural = _('9.2.1: If yes, please provide the new course details')
 
 
-class CourseReqDetailInline(admin.TabularInline):
+class CourseReqDetailInline(ReadOnlyMixin, admin.TabularInline):
     model = CourseReqDetail
     formset = CourseReqDetailFormSet
     extra = 1
@@ -158,7 +177,7 @@ class CourseReqDetailInline(admin.TabularInline):
     verbose_name_plural = _('9.5.1: If yes, please provide the course details.')
 
 
-class CollaboratorsInline(admin.TabularInline):
+class CollaboratorsInline(ReadOnlyMixin, admin.TabularInline):
     model = Collaborators
     formset = CollaboratorsFormSet
     extra = 1
@@ -327,7 +346,7 @@ class ReportingPeriodAdmin(admin.ModelAdmin):
 
 class ProjectDetailAdmin(admin.ModelAdmin):
     list_display = ('id', '__unicode__', 'record_status')
-    list_filter = (ReportingPeriodFilter, 'record_status')
+    list_filter = ('record_status',)
     form = ProjectDetailForm
     formfield_overrides = {
         models.ManyToManyField: {'widget': CheckboxSelectMultiple},
@@ -405,8 +424,11 @@ class ProjectDetailAdmin(admin.ModelAdmin):
             'fields': ('course_requirement', 'external_collaboration'),
             'description': ''
         }),
+        (None, {
+            'fields': (),
+            'description': ''
+        }),
     )
-
 
     class Media:
         js = ('javascript/app.js',)
@@ -428,7 +450,7 @@ class ProjectDetailAdmin(admin.ModelAdmin):
                 return True
             if user_has_perm(request, self.opts, 'view'):
                 return True
-        return False
+        return True
 
     def get_list_display(self, request):
         """
@@ -439,15 +461,22 @@ class ProjectDetailAdmin(admin.ModelAdmin):
             return list_display
         return self.list_display
 
+    def get_list_filter(self, request):
+        if not request.user.is_superuser:
+            self.list_filter.append(ReportingPeriodFilter)
+        return super(ProjectDetailAdmin, self).get_list_filter(request)
+
     def get_queryset(self, request):
-        qs = self.model.objects\
-                .filter(is_deleted=False)\
-                .filter(proj_leader__institute=get_user_institute(request.user))
-        if user_has_perm(request, self.opts, 'view'):
-            # Don't include draft records
-            return qs.exclude(record_status=1)
-        if user_has_perm(request, self.opts, 'change'):
-            return qs
+        if not request.user.is_superuser:
+            qs = self.model.objects\
+                    .filter(is_deleted=False)\
+                    .filter(proj_leader__institute=get_user_institute(request.user))
+            if user_has_perm(request, self.opts, 'view'):
+                # Don't include draft records
+                return qs.exclude(record_status=1)
+            if user_has_perm(request, self.opts, 'change'):
+                return qs
+        return super(ProjectDetailAdmin, self).get_queryset(request)
 
     def get_readonly_fields(self, request, obj=None):
         # For users with view access, all fields are readonly
@@ -458,7 +487,60 @@ class ProjectDetailAdmin(admin.ModelAdmin):
                     if field not in self.form.Meta.admin_editable:
                         readonly_fields.append(field)
             return readonly_fields
-        return self.readonly_fields
+
+    def get_form(self, request, obj=None, **kwargs):
+        # Global and institute admin have readonly views
+        if request.user.is_institute_admin() or request.user.is_superuser:
+            self.form = ProjectDetailAdminForm
+        return super(ProjectDetailAdmin, self).get_form(request, obj, **kwargs)
+
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name in ORG_LEVEL_FIELDS:
+            kwargs["queryset"] = db_field.related_model.objects.filter(
+                institute=get_user_institute(request.user))
+        return super(ProjectDetailAdmin, self).formfield_for_foreignkey(
+            db_field, request, **kwargs)
+
+    def formfield_for_manytomany(self, db_field, request, **kwargs):
+        if db_field.name == "strategic_objectives":
+            kwargs["queryset"] = StrategicObjective.objects.filter(
+                institute=get_user_institute(request.user))
+        return super(ProjectDetailAdmin, self).formfield_for_manytomany(
+            db_field, request, **kwargs)
+
+    def get_fieldsets(self, request, obj=None):
+        fieldsets = super(ProjectDetailAdmin, self).get_fieldsets(request, obj=obj)
+        if not request.user.is_superuser:
+            institute = get_user_institute(request.user)
+            field_list = []
+            for field in ORG_LEVEL_FIELDS:
+                if getattr(institute, '%s_name' % field) != '':
+                    field_list.append(field)
+            fields = tuple(field_list)
+            fieldsets[1][1]['fields'] = fields
+
+        if user_has_perm(request, self.opts, 'view'):
+            # Global + institute admin
+            fieldsets[len(fieldsets)-1][1]['fields'] = ('is_rejected', 'rejected_detail', 'is_flagged')
+        return fieldsets
+
+    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
+        form = context['adminform'].form
+        if form.fields.get('org_level_1', False):
+            if add:
+                institute = get_user_institute(request.user)
+            elif change:
+                institute = obj.institute
+
+            for field in ORG_LEVEL_FIELDS:
+                level_name = getattr(institute, '%s_name' % field)
+                if level_name != '':
+                    # If level_name is empty, formset doesn't have the fields included
+                    # See get_fieldsets() above
+                    form.fields[field].label = level_name
+
+        return super(ProjectDetailAdmin, self).render_change_form(
+            request, context, add=False, change=False, form_url='', obj=obj)
 
     def save_model(self, request, obj, form, change):
         """
@@ -508,65 +590,11 @@ class ProjectDetailAdmin(admin.ModelAdmin):
             obj.is_flagged = True
         obj.save()
 
-    def get_form(self, request, obj=None, **kwargs):
-        # Global and institute admin have readonly views
-        if request.user.is_institute_admin() or request.user.is_superuser:
-            self.form = ProjectDetailAdminForm
-        return super(ProjectDetailAdmin, self).get_form(request, obj, **kwargs)
-
-    def formfield_for_foreignkey(self, db_field, request, **kwargs):
-        if db_field.name in ORG_LEVEL_FIELDS:
-            kwargs["queryset"] = db_field.related_model.objects.filter(
-                institute=get_user_institute(request.user))
-        return super(ProjectDetailAdmin, self).formfield_for_foreignkey(
-            db_field, request, **kwargs)
-
-    def formfield_for_manytomany(self, db_field, request, **kwargs):
-        if db_field.name == "strategic_objectives":
-            kwargs["queryset"] = StrategicObjective.objects.filter(
-                institute=get_user_institute(request.user))
-        return super(ProjectDetailAdmin, self).formfield_for_manytomany(
-            db_field, request, **kwargs)
-
-    def get_fieldsets(self, request, obj=None):
-        fieldsets = super(ProjectDetailAdmin, self).get_fieldsets(request, obj=obj)
-        if not request.user.is_superuser:
-            institute = get_user_institute(request.user)
-            field_list = []
-            for field in ORG_LEVEL_FIELDS:
-                if getattr(institute, '%s_name' % field) != '':
-                    field_list.append(field)
-            fields = tuple(field_list)
-            fieldsets[1][1]['fields'] = fields
-        return fieldsets
-
-    def render_change_form(self, request, context, add=False, change=False, form_url='', obj=None):
-        form = context['adminform'].form
-        if form.fields:
-            # No fields on readonly view
-            if add:
-                institute = get_user_institute(request.user)
-            elif change:
-                institute = obj.institute
-
-            for field in ORG_LEVEL_FIELDS:
-                level_name = getattr(institute, '%s_name' % field)
-                if level_name != '':
-                    # If level_name is empty, formset doesn't have the fields included
-                    # See get_fieldsets() above
-                    form.fields[field].label = level_name
-
-        return super(ProjectDetailAdmin, self).render_change_form(
-            request, context, add=False, change=False, form_url='', obj=obj)
-
 
 admin.site.register(Institute, InstituteModelAdmin)
 admin.site.register(OrgLevel1, OrgLevelAdmin)
 admin.site.register(OrgLevel2, OrgLevelAdmin)
 admin.site.register(OrgLevel3, OrgLevelAdmin)
 admin.site.register(ReportingPeriod, ReportingPeriodAdmin)
-# admin.site.register(InstituteAdmin)
 admin.site.register(ProjectDetail, ProjectDetailAdmin)
-
-# admin.site.register(User, InstituteAdminUserAdmin)
 admin.site.register(CustomUser, CustomUserAdmin)
