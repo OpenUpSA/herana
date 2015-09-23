2b4b8d17f01173fdf496165387a0cc131bbc7957
Greg Kempe <greg@kempe.net>
2015-09-23 11:48:08 +0200

    Basics of static content pages

--- herana/urls.py
+++ herana/urls.py
@@ -5,7 +5,7 @@ from django.views.generic.base import RedirectView
 
 
 urlpatterns = patterns('',
-    url(r'^$', RedirectView.as_view(url='/admin/', permanent=True), name='home'),
+    url(r'^$', 'herana.views.home', name='home'),
     url(r'^grappelli/', include('grappelli.urls')),
     url(r'^accounts/', include('registration.backends.default.urls')),
 
